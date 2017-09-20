abc357d9fdbce88f1ecea26d663ec6907c14c9eb
Matteo Giaimo <mgiaimo@hawk.iit.edu>
2017-09-20 16:26:27 -0500

    huge update
    i have been messing with java script and i was able to get the submit button to display an alert when you click, and also after you leave a text box it auto changes all the letters to upper case but i will not use this for the email address part

---
+++ java.js
@@ -0,0 +1,15 @@
+function button()
+{
+   alert("Submit button clicked!");
+   return true;
+}
+
+function bigtext()
+{
+	var fnam= document.getElementById("firstName");
+	fnam.value = fnam.value.toUpperCase();
+}
+
+
+
+
